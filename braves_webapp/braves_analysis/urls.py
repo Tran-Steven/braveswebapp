eae523176b8b153c84f61aff30d498620606de79
Steven Tran <steven29160@gmail.com>
2024-10-16 18:40:59 -0400

    Clean

--- braves_webapp/braves_analysis/urls.py
+++ braves_webapp/braves_analysis/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('matchups.urls')),  # Ensure this line is present and correct
+    path('api/', include('matchups.urls')), 
 ]
 
