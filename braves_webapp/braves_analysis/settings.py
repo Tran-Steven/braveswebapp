88d0d54808926306c4b6a7f3dc4365c2136f07d3
Steven Tran <steven29160@gmail.com>
2024-10-16 18:19:21 -0400

    CORS

--- braves_webapp/braves_analysis/settings.py
+++ braves_webapp/braves_analysis/settings.py
@@ -36,7 +36,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', 
+]
 
 # Application definition
 
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
