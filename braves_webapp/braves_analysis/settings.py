6c54d85ac96074a1c4a5927030e7539e44ad11e2
Steven Tran <steven29160@gmail.com>
2024-10-16 18:16:35 -0400

    added CORSheader

--- braves_webapp/braves_analysis/settings.py
+++ braves_webapp/braves_analysis/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'corsheaders',
     'django.contrib.messages',
     'django.contrib.staticfiles',
         'rest_framework',
